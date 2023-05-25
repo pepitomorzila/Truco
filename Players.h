c7651ee2542fb12d87c057b193cbccdd9e413a25
pepito <pepitomorzila@gmail.com>
2023-05-25 11:18:32 -0300

    subo archivos

---
+++ Players.h
@@ -0,0 +1,9 @@
+#pragma once 
+#include <iostream>
+
+class players{
+    private:
+        int cant_cartas;
+
+
+};
---
+++ Reglas.h
@@ -0,0 +1,21 @@
+#include "Cartas.h"
+#pragma once
+#include <iostream>
+
+class reglas : public cartas
+{
+
+protected:
+    int tantos1;
+    int tantos2;
+    int valor_truco;
+    
+public:
+    reglas();
+    reglas(int [3], int [3], int [3], int [3], int, int, int);
+    reglas(int,int,int);
+    int Envido1(int[3]);
+    int Envido2(int[3]);
+    void Truco(int[3],int[3]);
+    int getTruco();
+};
